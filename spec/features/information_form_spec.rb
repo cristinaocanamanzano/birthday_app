732fed08682069d13df3d43491df31ea76755b10
Cristina María Ocaña Manzano <cristinaocana1@gmail.com>
2019-01-06 20:01:23 +0100

    User sees a form to fill out their name and birthday

--- spec/features/information_form_spec.rb
+++ spec/features/information_form_spec.rb
@@ -7,9 +7,13 @@ feature "Initial form" do
     expect(page).to have_content("Hello!")
   end
 
-  scenario "User is asked about their name and date of birth" do
+  scenario "User sees a form to fill out their name and birthday date" do
     visit '/'
     expect(page).to have_content("What is your name?")
-    expect(page).to have_content("What is your birthday?")
+    fill_in 'name', with: 'Cristina'
+    expect(page).to have_content("When is your birthday?")
+    fill_in 'day', with: '3'
+    page.select "October", :from => "month"
+    expect(page).to have_button("Send")
   end
 end
